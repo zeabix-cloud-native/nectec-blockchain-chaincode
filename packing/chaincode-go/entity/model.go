1c38d668e36580c767e3f40f45e08937a5617209
chattipong1 <chattipong.r@zeabix.com>
2024-04-05 10:22:41 +0700

    set struct not value nil

--- packing/chaincode-go/entity/model.go
+++ packing/chaincode-go/entity/model.go
@@ -1,18 +1,18 @@
 package entity
 
 type TransectionPacking struct {
-	Id             string  `json:"id"`
-	OrderID        string  `json:"orderId"`
-	FarmerID       string  `json:"farmerId"`
-	ForecastWeight *string `json:"forecastWeight"`
-	ActualWeight   *string `json:"actualWeight"`
-	IsPackerSaved  *bool   `json:"isPackerSaved"`
-	SavedTime      *string `json:"savedTime"`
-	IsApproved     *bool   `json:"isApproved"`
-	ApprovedDate   *string `json:"approvedDate"`
-	ApprovedType   *string `json:"approvedType"`
-	Owner          string  `json:"owner"`
-	OrgName        string  `json:"orgName"`
+	Id             string `json:"id"`
+	OrderID        string `json:"orderId"`
+	FarmerID       string `json:"farmerId"`
+	ForecastWeight string `json:"forecastWeight"`
+	ActualWeight   string `json:"actualWeight"`
+	IsPackerSaved  bool   `json:"isPackerSaved"`
+	SavedTime      string `json:"savedTime"`
+	IsApproved     bool   `json:"isApproved"`
+	ApprovedDate   string `json:"approvedDate"`
+	ApprovedType   string `json:"approvedType"`
+	Owner          string `json:"owner"`
+	OrgName        string `json:"orgName"`
 }
 
 type Pagination struct {
