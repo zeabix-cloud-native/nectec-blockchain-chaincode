bb0e7468be117c25dfffb7b35ef90de52908fb7e
chattipong1 <chattipong.r@zeabix.com>
2024-04-05 11:58:09 +0700

    chaincode exporter

---
+++ exporter/chaincode-go/entity/model.go
@@ -0,0 +1,13 @@
+package entity
+
+type TransectionExporter struct {
+	Id      string `json:"id"`
+	CertId  string `json:"certId"`
+	Owner   string `json:"owner"`
+	OrgName string `json:"orgName"`
+}
+
+type Pagination struct {
+	Skip  int `json:"skip"`
+	Limit int `json:"limit"`
+}
---
+++ exporter/chaincode-go/smart-contract/exporter.go
@@ -0,0 +1,296 @@
+package exporter
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/zeabix-cloud-native/nstda-blockchain-chaincode/exporter/chaincode-go/entity"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+func (s *SmartContract) CreateExporter(
+	ctx contractapi.TransactionContextInterface,
+	args string,
+) error {
+
+	var input entity.TransectionExporter
+
+	errInput := json.Unmarshal([]byte(args), &input)
+
+	if errInput != nil {
+		return fmt.Errorf("Unmarshal json string")
+	}
+
+	err := ctx.GetClientIdentity().AssertAttributeValue("exporter.creator", "true")
+	orgName, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return fmt.Errorf("submitting client not authorized to create asset, does not have exporter.creator role")
+	}
+
+	exists, err := s.AssetExists(ctx, input.Id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", input.Id)
+	}
+
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	asset := entity.TransectionExporter{
+		Id:      input.Id,
+		CertId:  input.CertId,
+		Owner:   clientID,
+		OrgName: orgName,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(input.Id, assetJSON)
+}
+
+func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface,
+	args string) error {
+
+	var input entity.TransectionExporter
+	errInput := json.Unmarshal([]byte(args), &input)
+
+	if errInput != nil {
+		return fmt.Errorf("Unmarshal json string")
+	}
+
+	asset, err := s.ReadAsset(ctx, input.Id)
+	if err != nil {
+		return err
+	}
+
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	if clientID != asset.Owner {
+		return fmt.Errorf("submitting client not authorized to update asset, does not own asset")
+	}
+	asset.Id = input.Id
+	asset.CertId = input.CertId
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(input.Id, assetJSON)
+}
+
+// DeleteAsset deletes a given asset from the world state.
+func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
+
+	asset, err := s.ReadAsset(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	if clientID != asset.Owner {
+		return fmt.Errorf("submitting client not authorized to update asset, does not own asset")
+	}
+
+	return ctx.GetStub().DelState(id)
+}
+
+func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newOwner string) error {
+
+	asset, err := s.ReadAsset(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	clientID, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	if clientID != asset.Owner {
+		return fmt.Errorf("submitting client not authorized to update asset, does not own asset")
+	}
+
+	asset.Owner = newOwner
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*entity.TransectionExporter, error) {
+
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the asset %s does not exist", id)
+	}
+
+	var asset entity.TransectionExporter
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Error creating exporter chaincode: %#c", asset)
+
+	return &asset, nil
+}
+
+func (s *SmartContract) GetAllExporter(ctx contractapi.TransactionContextInterface, args string) (*entity.GetAllReponse, error) {
+
+	orgName, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return nil, err
+	}
+
+	var input entity.Pagination
+	errInput := json.Unmarshal([]byte(args), &input)
+
+	if errInput != nil {
+		return nil, fmt.Errorf("Unmarshal json string")
+	}
+
+	limit := input.Limit
+	skip := input.Skip
+
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	total := 0
+	for resultsIterator.HasNext() {
+		_, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		total++
+	}
+
+	selector := map[string]interface{}{
+		"selector": map[string]interface{}{
+			"orgName": orgName,
+		},
+		"limit": limit,
+		"skip":  skip,
+	}
+
+	queryString, err := json.Marshal(selector)
+	if err != nil {
+		return nil, err
+	}
+
+	queryResults, _, err := ctx.GetStub().GetQueryResultWithPagination(string(queryString), int32(limit), "")
+	if err != nil {
+		return nil, err
+	}
+	defer queryResults.Close()
+
+	var assets []*entity.TransectionReponse
+
+	for queryResults.HasNext() {
+		queryResponse, err := queryResults.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset entity.TransectionReponse
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+
+		assets = append(assets, &asset)
+	}
+
+	if len(assets) == 0 {
+		assets = []*entity.TransectionReponse{}
+	}
+
+	return &entity.GetAllReponse{
+		Data:  "All Exporter",
+		Obj:   assets,
+		Total: total,
+	}, nil
+}
+
+// AssetExists returns true when asset with given ID exists in world state
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return assetJSON != nil, nil
+}
+
+func (s *SmartContract) GetSubmittingClientIdentity(ctx contractapi.TransactionContextInterface) (string, error) {
+
+	b64ID, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return "", fmt.Errorf("Failed to read clientID: %v", err)
+	}
+	decodeID, err := base64.StdEncoding.DecodeString(b64ID)
+	if err != nil {
+		return "", fmt.Errorf("failed to base64 decode clientID: %v", err)
+	}
+	return string(decodeID), nil
+}
+
+func (s *SmartContract) FilterExporter(ctx contractapi.TransactionContextInterface, key, value string) ([]*entity.TransectionExporter, error) {
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var assets []*entity.TransectionExporter
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset entity.TransectionExporter
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+
+		var m map[string]interface{}
+		if err := json.Unmarshal(queryResponse.Value, &m); err != nil {
+			return nil, err
+		}
+
+		if val, ok := m[key]; ok && fmt.Sprintf("%v", val) == value {
+			assets = append(assets, &asset)
+		}
+	}
+
+	return assets, nil
+}
