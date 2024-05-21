10a1a8666e39786c20d0cac274a81e0d1b2bd7c5
chattipong1 <chattipong.r@zeabix.com>
2024-05-21 15:22:01 +0700

    add  internal

---
+++ internal/issuer/core.go
@@ -0,0 +1,7 @@
+package issuer
+
+const (
+	UNAUTHORIZE string = "client is not authorized to delete this asset"
+	TIMEFORMAT  string = "2006-01-02T15:04:05Z"
+	SKIPOVER    string = "skip over total data"
+)
--- internal/issuer/issuer.go
+++ internal/issuer/issuer.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"time"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/zeabix-cloud-native/nstda-blockchain-chaincode/farmer/chaincode-go/entity"
 )
 
-// Unmarshal dynamically unmarshals JSON string into the provided entity type
 func Unmarshal(args string, entityType interface{}) (interface{}, error) {
 	entityValue := reflect.New(reflect.TypeOf(entityType)).Interface()
 	err := json.Unmarshal([]byte(args), entityValue)
@@ -15,6 +18,39 @@ func Unmarshal(args string, entityType interface{}) (interface{}, error) {
 	}
 	return entityValue, nil
 }
-func ExampleFunction() {
-	fmt.Println("This is an example function from the issuer package")
+
+func BuildQueryString(filter map[string]interface{}) (string, error) {
+
+	selector := map[string]interface{}{
+		"selector": filter,
+	}
+	queryString, err := json.Marshal(selector)
+	if err != nil {
+		return "", err
+	}
+	return string(queryString), nil
+}
+
+func CountTotalResults(ctx contractapi.TransactionContextInterface, queryString string) (int, error) {
+	resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
+	if err != nil {
+		return 0, err
+	}
+	defer resultsIterator.Close()
+
+	total := 0
+	for resultsIterator.HasNext() {
+		_, err := resultsIterator.Next()
+		if err != nil {
+			return 0, err
+		}
+		total++
+	}
+	return total, nil
+}
+
+func GetTimeNow() time.Time {
+	formattedTime := time.Now().Format(entity.TimeFormat)
+	CreatedAt, _ := time.Parse(entity.TimeFormat, formattedTime)
+	return CreatedAt
 }
